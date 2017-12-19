70b56661839cad41bf3fb57c28b51ee3f7b83ff5
adamzero1 <adam.crossland@zero1.co.uk>
2017-12-19 19:47:48 +0000

    Update entrypoint.sh

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -17,8 +17,12 @@ echo "password=${MYSQL_PASSWORD}" >> /home/${DEFAULT_USER}/.my.cnf
 echo "database=${MYSQL_DATABASE}" >> /home/${DEFAULT_USER}/.my.cnf
 
 # Git Config
-git config user.email "${GIT_EMAIL}"
-git config user.name "${GIT_NAME}"
+if [ ! -z $GIT_EMAIL ]; then
+  git config user.email "${GIT_EMAIL}"
+fi
+if [ ! -z $GIT_NAME ]; then
+  git config user.name "${GIT_NAME}"
+fi
 
 chown -R ${DEFAULT_USER}:${DEFAULT_USER} /home/${DEFAULT_USER}
 
